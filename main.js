12323d3f05bc4ab4b439b445a799ae8cf2490c1d
jackie <lazmonbiro@gmail.com>
2022-10-23 00:37:38 +0700

    init

--- main.js
+++ main.js
@@ -1,59 +1,38 @@
-let N = prompt('')
-N = parseInt(N)
-for (i = 0; i < N; i++) {
-	for (j = 0; j <= i; j++) {
-		document.writeln(" * ")
-	}
-	document.writeln("<br/>")
-}
+// let N = prompt('')
+// N = parseInt(N)
+// for (i = 0; i < N; i++) {
+// 	for (j = 0; j <= i; j++) {
+// 		document.writeln(" * ")
+// 	}
+// 	document.writeln("<br/>")
+// }
 
 
 
 let $a = document.querySelector('.a');
 let $b = document.querySelector('.b');
+let $btn = document.querySelector('.btn');
 
 
 
-	let parser = (str) => {
-	let arr = [];
-	let parts = str.split('-');
-	let from = Math.min(parseInt(parts[0]), parseInt(parts[1]));
-	let to = Math.max(parseInt(parts[0]), parseInt(parts[1]));
-	for (let i = from; i <= to; i++) {
-		arr.push(i);
+$btn.addEventListener("click", (e) => {
+	console.log(parser($a.value, $b.value));
+	console.log($a.value, $b.value);
+
 	}
+)
+
+
+
+let parser = (a, b) => {
+let arr = [];
+let from = Math.min(a[0]);
+let to = Math.max(b[1]);
+for (let i = from; i <= to; i++) {
+	arr.push(i);
+}
 	return arr;
 }
 
-console.log(parser('$a-$b'));
+console.log(parser('1', '18'));
 
-function kiem_tra_snt(n) {
-	var flag = true;
-	if (n < 2) {
-		flag = false;
-	}
-	else if (n == 2) {
-		flag = true;
-	}
-	else if (n % 2 == 0) {
-		flag = false;
-	}
-	else {
-		for (var i = 3; i < Math.sqrt(n); i += 2) {
-			if (n % i == 0) {
-				flag = false;
-				break;
-			}
-		}
-	}
-
-
-	if (flag == true) {
-		document.write(n + " là số nguyên tố <br/>");
-	}
-	else {
-		document.write(n + " không phải là số nguyên tố <br/>");
-	}
-}
-
-kiem_tra_snt(parser);
